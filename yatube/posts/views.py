4708f3c3e28a938cb962c6a74350ea01fcfcdced
Никита Лукьянчук <nikluk@mail.ru>
2022-11-07 14:13:42 +0300

    Yatube - blog platform, social network (ver 19.0)

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -149,8 +149,8 @@ def follow_index(request):
 def profile_follow(request, username):
     """Function help to subscribe the user to author."""
     auth = get_object_or_404(User, username=username)
-    if not (
-        Follow.objects.filter(user=request.user, author=auth).exists()
+    if (
+        not (Follow.objects.filter(user=request.user, author=auth).exists())
         and auth != request.user
     ):
         Follow.objects.create(user=request.user, author=auth)
